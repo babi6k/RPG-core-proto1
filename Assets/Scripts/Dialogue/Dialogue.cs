76d2d6037173b3924dd398dd2a9785e1e45ca2eb
Babi6k <yaniv.hayat1@gmail.com>
2021-01-24 08:58:53 +0700

    Added Dialogue Editor for creating Quest system
    Dialogue editor with dailogue node and dialogue class

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RPG.Dialogue
+{
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField]
+        List<DialogueNode> nodes = new List<DialogueNode>();
+
+        Dictionary<string, DialogueNode> nodeLookup = new Dictionary<string, DialogueNode>();
+
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if (nodes.Count == 0)
+            {
+                CreateNode(null);
+            }
+            OnValidate();
+        }
+#endif
+        private void OnValidate()
+        {
+            nodeLookup.Clear();
+            foreach (DialogueNode node in GetAllNodes())
+            {
+                nodeLookup[node.name] = node;
+            }
+        }
+
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+
+        public DialogueNode GetRootNode()
+        {
+            return nodes[0];
+        }
+
+        public IEnumerable<DialogueNode> GetAllChildren(DialogueNode parentNode)
+        {          
+            foreach(string childID in parentNode.children)
+            {
+                if (nodeLookup.ContainsKey(childID))
+                {
+                    yield return nodeLookup[childID];
+                }
+            }
+        }
+
+        public void CreateNode(DialogueNode parent)
+        {
+            DialogueNode newNode = CreateInstance<DialogueNode>();
+            newNode.name = System.Guid.NewGuid().ToString();
+            Undo.RegisterCreatedObjectUndo(newNode, "Created Dialogue Node");
+            if (parent != null) 
+            {
+                parent.children.Add(newNode.name);
+            }
+            nodes.Add(newNode);
+            OnValidate();
+        }
+
+        public void DeleteNode(DialogueNode nodeToDelete)
+        {
+            nodes.Remove(nodeToDelete);
+            OnValidate();
+            foreach (DialogueNode node in GetAllNodes())
+            {
+                node.children.Remove(nodeToDelete.name);
+            }
+            Undo.DestroyObjectImmediate(nodeToDelete);
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    
+    public class DialogueNode : ScriptableObject
+    { 
+        public string text;
+        public List<string> children = new List<string>();
+        public Rect rect = new Rect(0,0,300,100); 
+    }
+}
