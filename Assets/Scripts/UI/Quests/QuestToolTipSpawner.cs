1159c67d4d6a4019920c091e61dbd0cf78785b18
Babi6k <yaniv.hayat1@gmail.com>
2021-02-19 19:57:56 +0700

    New UI Design Add button menu

---
+++ Assets/Scripts/UI/Quests/QuestToolTipSpawner.cs
@@ -0,0 +1,18 @@
+﻿using GameDevTV.Core.UI.Tooltips;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class QuestToolTipSpawner : TooltipSpawner
+    {
+        public override bool CanCreateTooltip()
+        {
+            return true;
+        }
+
+        public override void UpdateTooltip(GameObject tooltip)
+        {
+           
+        }
+    }
+}
