2ba204b83421758f6e38dfe61b1648c3a98a3dcd
Babi6k <yaniv.hayat1@gmail.com>
2021-04-22 10:37:24 +0700

    Added A shop UI opening and closing shop from the shopKeeper prefab clicking on it and closing with close button

---
+++ Assets/Scripts/Shops/Shop.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Inventories;
+using RPG.Control;
+using UnityEngine;
+
+namespace RPG.Shops
+{
+    public class Shop : MonoBehaviour,IRaycastable
+    {
+        [SerializeField] string shopName;
+
+        public class ShopItem
+        {
+            InventoryItem item;
+            int availability;
+            float price;
+            int quantityInTransaction;
+        }
+
+        public event Action onChange;
+
+        public string GetShopName() {return shopName;}
+
+        public IEnumerable<ShopItem> GetFilteredItems()
+        {
+            return null;
+        }
+
+        public void SelectFilter(ItemCategory category)
+        {
+
+        }
+
+        public ItemCategory GetFilter() {return ItemCategory.None;}
+
+        public void SelectMode(bool isBuying)
+        {
+
+        }
+
+        public bool IsBuyingMode()
+        {
+            return true;
+        }
+
+        public bool CanTransact() 
+        {
+            return true;
+        }
+
+        public void ConfirmTransaction()
+        {
+
+        }
+
+        public float TransactionTotal()
+        {
+            return 0;
+        }
+
+        public void AddToTransaction(InventoryItem item, int quantitiy)
+        {
+
+        }
+
+        public CursorType GetCursorType()
+        {
+            return CursorType.Shop;
+        }
+
+        public bool HandleRayCast(PlayerController callingController)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                callingController.GetComponent<Shopper>().SetActiveShop(this);
+            }
+            return true;
+        }
+    }
+}
